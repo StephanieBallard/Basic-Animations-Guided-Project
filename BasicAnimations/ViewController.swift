1e48ac7f059ec8313a2c311c21a82b275375d178
Stephanie Ballard <user.name>
2020-04-14 10:25:01 -0700

    .

---
+++ BasicAnimations/ViewController.swift
@@ -0,0 +1,114 @@
+//
+//  ViewController.swift
+//  BasicAnimations
+//
+//  Created by Ben Gohlke on 4/14/20.
+//  Copyright © 2020 Lambda School. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    var label = UILabel()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemBackground
+        configureLabel()
+        configureButtons()
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        // Places label in middle of view container (both x and y axis)
+        label.center = view.center
+    }
+
+    private func configureLabel() {
+        label = UILabel(frame: CGRect(x: 0, y: 0, width: 200, height: 200))
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.layer.borderWidth = 2
+        label.layer.cornerRadius = 12
+        label.text = "👨‍🏫"
+        label.textAlignment = .center
+        label.font = UIFont.systemFont(ofSize: 96)
+        label.widthAnchor.constraint(equalTo: label.heightAnchor).isActive = true
+        view.addSubview(label)
+    }
+    
+    private func configureButtons() {
+        let rotateButton = UIButton(type: .system)
+        rotateButton.translatesAutoresizingMaskIntoConstraints = false
+        rotateButton.setTitle("Rotate", for: .normal)
+        rotateButton.addTarget(self, action: #selector(rotateButtonTapped), for: .touchUpInside)
+        
+        let springButton = UIButton(type: .system)
+        springButton.translatesAutoresizingMaskIntoConstraints = false
+        springButton.setTitle("Spring", for: .normal)
+        springButton.addTarget(self, action: #selector(springButtonTapped), for: .touchUpInside)
+        
+        let keyButton = UIButton(type: .system)
+        keyButton.translatesAutoresizingMaskIntoConstraints = false
+        keyButton.setTitle("Key", for: .normal)
+        keyButton.addTarget(self, action: #selector(keyButtonTapped), for: .touchUpInside)
+        
+        let stackView = UIStackView()
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        stackView.axis = .horizontal
+        stackView.distribution = .equalSpacing
+        view.addSubview(stackView)
+        
+        stackView.addArrangedSubview(rotateButton)
+        stackView.addArrangedSubview(springButton)
+        stackView.addArrangedSubview(keyButton)
+        
+        
+        NSLayoutConstraint.activate([
+            stackView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 20),
+            stackView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -20),
+            stackView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -20)
+        ])
+    }
+    
+    @objc private func rotateButtonTapped() {
+        label.center = view.center
+        
+        UIView.animate(withDuration: 2.0, animations: {
+            self.label.transform = CGAffineTransform(rotationAngle: .pi/4)
+        }) { _ in
+            UIView.animate(withDuration: 2.0) {
+                self.label.transform = .identity
+            }
+        }
+    }
+    
+    @objc private func springButtonTapped() {
+        label.center = view.center
+        label.transform = CGAffineTransform(scaleX: 0.0001, y: 0.0001)
+        UIView.animate(withDuration: 3.0,
+                       delay: 0,
+                       usingSpringWithDamping: 0.3,
+                       initialSpringVelocity: 0,
+                       options: [],
+                       animations: {
+            self.label.transform = .identity
+        },             completion: nil)
+    }
+    
+    @objc private func keyButtonTapped() {
+        label.center = view.center
+        
+        UIView.animateKeyframes(withDuration: <#T##TimeInterval#>, delay: <#T##TimeInterval#>, options: <#T##UIView.KeyframeAnimationOptions#>, animations: <#T##() -> Void#>, completion: <#T##((Bool) -> Void)?##((Bool) -> Void)?##(Bool) -> Void#>)
+    }
+    
+    @objc private func squashButtonTapped() {
+        
+    }
+    
+    @objc private func anticipationButtonTapped() {
+        
+    }
+}
+
